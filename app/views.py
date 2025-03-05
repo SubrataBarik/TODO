dbffb9e9261e579a62b7ef67c65b9ffc8a62c16a
SubratBarik <bariksubrat021@gmail.com>
2025-03-05 16:13:21 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import *
+# Create your views here.
+
+def home(request):
+    all_todos=Todo.objects.all()
+    d={'all_todos':all_todos}
+    if request.method == "POST":
+        title=request.POST.get('title')
+        desc=request.POST.get('desc')
+        TO=Todo(title=title , desc=desc)
+        TO.save()
+    return render(request, 'home.html' ,d)
