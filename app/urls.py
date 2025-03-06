ee4291ed7ff7a302009520be72337c1e3d508b6e
SubratBarik <bariksubrat021@gmail.com>
2025-03-06 11:06:05 +0530

    first commit

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from .views import *
 
+
 urlpatterns = [
-    path("" ,home , name='home'),
-    path('update_todo/', update_todo, name='update_todo'),
-    path('delete_todo/', delete_todo, name='delete_todo'),
-
-
+    path('', home, name='home'),
+    path('update<pk>', update, name='update'),
+    path('delete<pk>', delete, name='delete')
+]
--- app/views.py
+++ app/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from .models import *
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
-
 def home(request):
-    all_todos=Todo.objects.all()
-    d={'all_todos':all_todos}
-    if request.method == "POST":
-        title=request.POST.get('title')
-        desc=request.POST.get('desc')
-        TO=Todo(title=title , desc=desc)
+    all_todos = Todo.objects.all()
+    d = {'all_todos': all_todos}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        desc = request.POST.get('desc')
+        TO = Todo(title=title, desc=desc)
         TO.save()
-    return render(request, 'home.html' ,d)
+    return render(request, 'home.html', d)
 
-def  update(request, pk):
+
+def update(request, pk):
     TO = Todo.objects.get(pk=pk)
     d = {'TO': TO}
     if request.method == 'POST':
@@ -24,6 +26,7 @@ def  update(request, pk):
         TO.save()
         return HttpResponseRedirect(reverse('home'))
     return render(request, 'update.html', d)
+
 
 def delete(request, pk):
     TO = Todo.objects.get(pk=pk)
